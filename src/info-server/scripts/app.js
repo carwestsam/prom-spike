887e514622184bb0b7b275cc0ab29a27fc872fd5
Cedric Sung <carwestsam@gmail.com>
2019-01-19 11:58:21 +0800

    fire alert from prometheus

--- src/info-server/scripts/app.js
+++ src/info-server/scripts/app.js
@@ -2,13 +2,19 @@ const http = require("http");
 const port = 3000;
 var date = new Date();
 let start_timestamp = date.getTime();
+let alert = 1.0
 
 const requestHandler = (request, response) => {
+
+  if (request.url == '/alert') {
+     alert = 1.01 - alert
+  }
+
   let d = new Date();
   current_timestamp = d.getTime();
   let diff = (current_timestamp - start_timestamp) / 10000
   response.end(
-    `app_melt_volt{side="0.1"} ${1.0 + Math.sin(diff)}`
+    `app_melt_volt{side="0.1"} ${ (1.0 + Math.sin(diff) ) * alert }`
   );
 };
 
